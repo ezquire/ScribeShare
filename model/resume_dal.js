8761c87b31da9c6c271d0ae43c2aa28833fd1df9
ezquire <tyler.gearing@gmail.com>
2017-04-21 11:23:55 -0700

    working on lab 11 multiple value boxes

--- model/resume_dal.js
+++ model/resume_dal.js
@@ -25,11 +25,10 @@ exports.getUser = function(account_id, callback) {
 exports.insert = function(params, callback) {
 
     // FIRST INSERT THE COMPANY
-    var query = 'INSERT INTO company (company_name) VALUES (?)';
+    var query = 'INSERT INTO resume (resume_name) VALUES (?)';
+    var queryData = [params.resume_name];
 
-    var queryData = [params.company_name];
-
-    connection.query(query, params.company_name, function(err, result) {
+    connection.query(query, queryData, function(err, result) {
 
         // THEN USE THE COMPANY_ID RETURNED AS insertId AND THE SELECTED ADDRESS_IDs INTO COMPANY_ADDRESS
         var company_id = result.insertId;
@@ -38,15 +37,7 @@ exports.insert = function(params, callback) {
         var query = 'INSERT INTO company_address (company_id, address_id) VALUES ?';
 
         // TO BULK INSERT RECORDS WE CREATE A MULTIDIMENSIONAL ARRAY OF THE VALUES
-        var companyAddressData = [];
-        for(var i=0; i < params.address_id.length; i++) {
-            companyAddressData.push([company_id, params.address_id[i]]);
-        }
 
-        // NOTE THE EXTRA [] AROUND companyAddressData
-        connection.query(query, [companyAddressData], function(err, result){
-            callback(err, result);
-        });
     });
 
 };
