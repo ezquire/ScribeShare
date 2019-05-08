d6d596ab146aad2a3b3b2bd6ed789d9ad5dc46d9
ezquire <tyler.gearing@gmail.com>
2019-05-08 12:00:54 -0700

    added landing pages with submit buttons and ratings for the first three note sets, created thank you page

--- routes/browse_routes.js
+++ routes/browse_routes.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res) {
     res.render('browse/browseAll');
 });
 
+// View individual notes (really this should be one page that gets rendered with a back-end)
+router.get('/activation', function(req, res) {
+    res.render('notes/activation');
+});
+
+router.get('/compilers', function(req, res) {
+    res.render('notes/compilers');
+});
+
+router.get('/hashing', function(req, res) {
+    res.render('notes/hashing');
+});
+
+// Thank you for submitting page
+router.get('/submit', function(req, res) {
+    res.render('notes/submit');
+});
+
 module.exports = router;
