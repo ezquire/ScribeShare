d6dba6d2090f2a2f4b0a6cae2d32dce079e20285
ezquire <tyler.gearing@gmail.com>
2019-05-08 22:54:24 -0700

    final touches to make it look presentable

--- routes/upload_routes.js
+++ routes/upload_routes.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
     res.render('upload/upload');
 });
 
+router.get('/thank', function(req, res) {
+    res.render('upload/thank');
+});
+
 module.exports = router;
