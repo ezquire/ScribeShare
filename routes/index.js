e1fdd252d6f552ad648e0eadba030a1b3bc12b9a
ezquire <tyler.gearing@gmail.com>
2019-10-15 21:08:58 -0700

    working on the navbar

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Notebook - Crowd Sourced Note Sharing' });
+  res.render('index', { title: 'ScribeShare' });
 });
 
 module.exports = router;
